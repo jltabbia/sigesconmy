277479cdebbb97c09251adb8dcf4f01e4c2a495d
Joselo Tabbia <jltabbia@gmail.com>
2022-10-31 15:39:24 -0300

    casa

--- sigescon/urls.py
+++ sigescon/urls.py
@@ -17,13 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.decorators import login_required
-from .views import HomeView, cerrarSesion
+from .views import *
 from propietarios.urls import *
 from edificios.urls import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',HomeView,name='index'),
+    path('home/<int:id>',ingreso,name='home'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('salir/',cerrarSesion,name='salir'),
     path('propietarios', include('propietarios.urls', namespace='propietarios')),
--- sigescon/views.py
+++ sigescon/views.py
@@ -2,13 +2,20 @@ from django.shortcuts import render, redirect
 from django.views.generic import View
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from edificios.models import Edificios
 
 # Create your views here.
 @login_required()
 
 def HomeView(request):
-       
-    return render(request,'index.html',{})
+    edificios=Edificios.objects.filter(administrador=request.user) 
+    return render(request,'ingreso.html',{'edificios':edificios})
+
+def ingreso(request,id):
+    
+    edificio=Edificios.objects.all()
+    print(edificio)
+    return render(request,'index.html',{'edificio':edificio})
 
 def cerrarSesion(request):
     logout(request)
